0bb19ca2f7b185224ce0fd63ca1403cb2ac2a51e
JIeunhuh <entks301@naver.com>
2026-05-14 17:17:24 +0900

    빌드 성공 후 pm2 띄우도록 스크립트 수정

--- sbdBackStart.bat
+++ sbdBackStart.bat
@@ -5,15 +5,17 @@ git reset --hard
 echo git reset
 git pull origin master
 echo git pull
-call pm2 stop sbd_back
-echo pm2 stop
-powershell -nop -c "& {sleep 5}"
-echo sleep 5 done
+
 call gradlew.bat clean
 echo gradle clean
 call gradlew.bat build
+if errorlevel 1 exit /b 1
 echo gradle build
+
 powershell -nop -c "& {sleep 10}"
 echo sleep 10 done
+
+call pm2 delete sbd_back
+echo pm2 delete
 call pm2 start java --name sbd_back -- -jar build/libs/ems-0.0.1-SNAPSHOT.war
 echo pm2 start
