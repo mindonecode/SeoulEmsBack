a6e4266795dba539d62a7055be6e9fe8c5c6c565
JIeunhuh <entks301@naver.com>
2026-05-18 15:50:27 +0900

    배포 test plz

--- sbdBackStart.bat
+++ sbdBackStart.bat
@@ -2,20 +2,21 @@
 cd /d D:\SBD_Proj\SeoulEmsBack\
 
 set JAVA_HOME=C:\Program Files\Java\jdk-17
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
 set NODE_EXE=D:\Program Files\nodejs\node.exe
 set PM2_CMD=C:\Users\hjseok\AppData\Roaming\npm\pm2.cmd
-set PATH=%JAVA_HOME%\bin;%NODE_HOME%;%PATH%
+set PATH=%JAVA_HOME%\bin;D:\Program Files\nodejs;%PATH%
 
 echo ===== ENV CHECK START =====
+echo GIT_EXE=%GIT_EXE%
 echo JAVA_HOME=%JAVA_HOME%
+echo JAVA_EXE=%JAVA_EXE%
 echo NODE_EXE=%NODE_EXE%
 echo PM2_CMD=%PM2_CMD%
-where java
-java -version
+"%JAVA_EXE%" -version
 javac -version
 "%NODE_EXE%" -v
 call "%PM2_CMD%" -v
-call gradlew.bat --version
 echo ===== ENV CHECK END =====
 
 git reset --hard
@@ -35,5 +36,5 @@ echo gradle build
 powershell -nop -c "& {sleep 10}"
 echo sleep 10 done
 
-call "%PM2_CMD%" start java --name sbd_back -- -jar build/libs/ems-0.0.1-SNAPSHOT.war
+call "%PM2_CMD%" start cmd --name sbd_back -- /c ""%JAVA_EXE%" -jar D:\SBD_Proj\SeoulEmsBack\build\libs\ems-0.0.1-SNAPSHOT.war"
 echo pm2 start
