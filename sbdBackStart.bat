0f69b605ba254a9890c814913804994cfa5abbe2
JIeunhuh <entks301@naver.com>
2026-05-18 15:55:27 +0900

    배포 test plz 2

--- sbdBackStart.bat
+++ sbdBackStart.bat
@@ -1,30 +1,29 @@
 @echo on
 cd /d D:\SBD_Proj\SeoulEmsBack\
+if errorlevel 1 exit /b 1
 
 set JAVA_HOME=C:\Program Files\Java\jdk-17
 set JAVA_EXE=%JAVA_HOME%\bin\java.exe
 set NODE_EXE=D:\Program Files\nodejs\node.exe
-set PM2_CMD=C:\Users\hjseok\AppData\Roaming\npm\pm2.cmd
 set PATH=%JAVA_HOME%\bin;D:\Program Files\nodejs;%PATH%
 
 echo ===== ENV CHECK START =====
-echo GIT_EXE=%GIT_EXE%
 echo JAVA_HOME=%JAVA_HOME%
 echo JAVA_EXE=%JAVA_EXE%
 echo NODE_EXE=%NODE_EXE%
-echo PM2_CMD=%PM2_CMD%
 "%JAVA_EXE%" -version
-javac -version
 "%NODE_EXE%" -v
-call "%PM2_CMD%" -v
+call "C:\Users\hjseok\AppData\Roaming\npm\pm2.cmd" -v
+call gradlew.bat --version
 echo ===== ENV CHECK END =====
 
 git reset --hard
 echo git reset
 git pull origin master
+if errorlevel 1 exit /b 1
 echo git pull
 
-call "%PM2_CMD%" delete sbd_back
+call "C:\Users\hjseok\AppData\Roaming\npm\pm2.cmd" delete sbd_back
 echo pm2 delete
 
 call gradlew.bat clean
@@ -36,5 +35,5 @@ echo gradle build
 powershell -nop -c "& {sleep 10}"
 echo sleep 10 done
 
-call "%PM2_CMD%" start cmd --name sbd_back -- /c ""%JAVA_EXE%" -jar D:\SBD_Proj\SeoulEmsBack\build\libs\ems-0.0.1-SNAPSHOT.war"
+call "C:\Users\hjseok\AppData\Roaming\npm\pm2.cmd" start cmd --name sbd_back -- /c ""%JAVA_EXE%" -jar D:\SBD_Proj\SeoulEmsBack\build\libs\ems-0.0.1-SNAPSHOT.war"
 echo pm2 start
