4cbab3dc7d5336a485497c71d518f2d4151aa4cf
thchoi <xogh2120@gmail.com>
2026-06-05 15:26:53 +0900

    부하별 제어조건 작업중

--- src/main/java/kr/co/mindone/ems/common/CommonMapper.java
+++ src/main/java/kr/co/mindone/ems/common/CommonMapper.java
@@ -174,4 +174,10 @@ public interface CommonMapper {
      * @return 직전 태그 데이터
      */
     List<HashMap<String, Object>> selectWtlvData(HashMap<String, Object> tagIds);
+
+    /**
+     * 런타임 설정 전체 조회 (TB_CONFIG). AppConfigStore 캐시 적재용.
+     * @return CFG_KEY / CFG_VAL 목록
+     */
+    List<HashMap<String, Object>> selectAppConfig();
 }
